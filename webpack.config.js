e81eeed3aa79609949f2c798f047b293d17622a5
Vincent Laurensius <vincenthambaya@gmail.com>
2023-03-28 08:21:44 +0700

    [FEAT] Webpack enabler multiple output and entry

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,12 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+  entry: {
+    index: "./src/index.js",
+    product: "./src/products.js",
   },
-  module: {
-    rules: [
-      {
-        // only match .css
-        test: /\.(css)$/,
-        use: [
-          { loader: "style-loader" },
-          // enable unique classname to prevent overrides class
-          { loader: "css-loader", options: { modules: true } },
-        ],
-      },
-      {
-        // work with ".sass" or ".scss" filte
-        test: /.s[ac]ss$/,
-        use: [
-          { loader: "style-loader" },
-          { loader: "css-loader", options: { modules: true } },
-          { loader: "sass-loader" },
-        ],
-      },
-    ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
 };
