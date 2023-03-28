60a54de6dcadc4cd8c36d7c4b6a0254df595022d
Vincent Laurensius <vincenthambaya@gmail.com>
2023-03-28 08:22:48 +0700

    [UI] Interface products and index page

--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
 import _ from "lodash";
-import style from "./index.scss";
-import "./clearButton";
+import newArrivalData from "./newArrival.json";
 
-console.log(style);
-
-const btn1 = document.getElementById("button1");
-
-btn1.addEventListener("click", function () {
-  const el = document.getElementById("header");
-  el.innerHTML = "Hey i have updated the code !";
-
-  const listItems = ["Apple", "orange", "Banana"];
-  const ul = document.getElementById("shoppingList");
-  _.forEach(listItems, function (item) {
-    const tempEl = document.createElement("li");
-    tempEl.innerHTML = item;
-    ul.appendChild(tempEl);
+function renderItems(itemArr) {
+  const cakeContainerEl = document.getElementById("cakesContainer");
+  cakeContainerEl.innerHTML = ``;
+  _.forEach(itemArr, (item) => {
+    let cakeEl = document.createElement("div");
+    cakeEl.classList.add("cakes-container");
+    cakeEl.innerHTML = `
+    <img src="./assets/${item.imgName}" />
+    <strong>${item.title}</strong>
+    <span>${item.price}</span>
+    `;
+    cakeContainerEl.appendChild(cakeEl);
   });
-});
+}
 
-btn1.classList.add([style.button]);
+document
+  .getElementById("categorySelect")
+  .addEventListener("change", function (e) {
+    const selected = e.target.value;
+    if (selected === "all") {
+      renderItems(newArrivalData);
+    } else {
+      let filteredData = _.filter(newArrivalData, function (item) {
+        return item.category === selected;
+      });
+      renderItems(filteredData);
+    }
+  });
+
+renderItems(newArrivalData);
